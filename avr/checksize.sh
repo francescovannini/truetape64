9679ee4007076d66fb897ce753baf5814ac61116
francesco <vannini@gmail.com>
2022-05-26 18:35:59 +0200

    Build tools cleanup

--- avr/checksize.sh
+++ avr/checksize.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/env bash
-target="truetape64"
+#!/bin/bash
 codelimit=2048
 datalimit=128
+TARGET="$1"
 
-set -- `avr-size -d "$target" | awk '/[0-9]/ {print $1 + $2, $2 + $3, $2}'`
+set -- $(avr-size -d "$TARGET"| awk '/[0-9]/ {print $1 + $2, $2 + $3, $3}')
 if [ $1 -gt $codelimit ]; then
-	echo "Error: code size $1 exceeds limit of $codelimit"
-	exit 1
+  echo "Error: code size $1 exceeds limit of $codelimit"
+  exit 1
 else
-	echo "Code: $1 bytes (data=$3)"
+  echo "Code: $1 bytes (data=$3)"
 fi
 
 if [ $2 -gt $datalimit ]; then
-	echo "Error: data size $2 exceeds limit of $datalimit"
-	exit 1
+  echo "Error: data size $2 exceeds limit of $datalimit"
+  exit 1
 else
-	echo "Data: $2 bytes"
+  echo "Data: $2 bytes"
 fi
+
+nm --print-size --size-sort -r --radix=d "$TARGET"
--- avr/flash.sh
+++ avr/flash.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 target="truetape64"
 target_hex=$target".hex"
-programlimit=2048
-datalimit=128
 
 if [ "$1" == "-b" ]; then
   rm -rf cmake-build-release && mkdir cmake-build-release && cd cmake-build-release || exit 1
   cmake -DCMAKE_BUILD_TYPE=Release ..
   make
-
-  set -- `avr-size "$target" | awk '/[0-9]/ {print $1 + $2, $2 + $3, $2}'`
-  if [ $1 -gt $programlimit ]; then
-    echo "Error: program size $1 exceeds limit of $programlimit"
-    exit 1
-  else
-    echo "Program: $1 bytes (data=$3)"
-  fi
-
-  if [ $2 -gt $datalimit ]; then
-    echo "Error: data size $2 exceeds limit of $datalimit"
-    exit 1
-  else
-    echo "Data: $2 bytes"
-  fi
-
   avr-objcopy -O ihex $target $target_hex
 else
   cd cmake-build-release || exit 1
